fdaeaeb2077691601b82cfdc12736eb2279520a6
PC Luciano Di Pietro <huesodipietro@gmail.com>
2026-01-15 00:49:17 -0300

    creado el servicio y controlador para recibir los archivos excel y actualizar la base de datos desde el endpoint

--- tools/add_data.py
+++ tools/add_data.py
@@ -28,20 +28,20 @@ def procesar_archivos(file_socios, file_deudas):
                     nuevos_nombres[cols[c]] = 'cuit'
                     encontrados.add('cuit')
                 # if 'nombre' == c:
-                if 'nombre_apellido' not in encontrados and any(x in c for x in ['cliente' ,'nombre' ,'socio' ,'apellido']):
-                    nuevos_nombres[cols[c]] = 'nombre_apellido'
-                    encontrados.add('nombre_apellido')
+                if 'nombre' not in encontrados and any(x in c for x in ['cliente' ,'nombre' ,'socio' ,'apellido']):
+                    nuevos_nombres[cols[c]] = 'nombre'
+                    encontrados.add('nombre')
                 if 'telefono' not in encontrados and any(x in c for x in ['tel' ,'telefono' ,'cel' ,'celular']):
                     nuevos_nombres[cols[c]] = 'telefono'
                     encontrados.add('telefono')
         
         elif tipo == 'deudas':
             for c in cols:
-                if 'nombre_apellido' not in encontrados and c =='cliente':
-                    nuevos_nombres[cols[c]] = 'nombre_apellido'
-                    encontrados.add('nombre_apellido')
-                if 'monto_deuda' not in encontrados and c == 'importe mon. ppal.':
-                    nuevos_nombres[cols[c]] = 'monto_deuda'
+                if 'nombre' not in encontrados and c =='cliente':
+                    nuevos_nombres[cols[c]] = 'nombre'
+                    encontrados.add('nombre')
+                if 'monto_adeudado' not in encontrados and c == 'importe mon. ppal.':
+                    nuevos_nombres[cols[c]] = 'monto_adeudado'
                     encontrados.add('monto_deuda')
         
         return df.rename(columns=nuevos_nombres)
@@ -49,7 +49,7 @@ def procesar_archivos(file_socios, file_deudas):
     # Aplicamos la flexibilidad
     df_socios = estandarizar_columnas(df_socios, 'socios')
 
-    df_socios['nombre_apellido'] = df_socios['nombre_apellido'].astype(str).str.strip().str.upper()
+    df_socios['nombre'] = df_socios['nombre'].astype(str).str.strip().str.upper()
 
     df_socios['cuit'] = df_socios['cuit'].astype(str).str.replace('.0', '', regex=False)
 
@@ -58,22 +58,22 @@ def procesar_archivos(file_socios, file_deudas):
         df_deudas = pd.read_excel(path_d) if file_deudas.filename.endswith('.xlsx') else pd.read_csv(path_d)
         df_deudas = estandarizar_columnas(df_deudas, 'deudas')
         
-        df_deudas['nombre_apellido'] = df_deudas['nombre_apellido'].astype(str).str.strip().str.upper()
+        df_deudas['nombre'] = df_deudas['nombre'].astype(str).str.strip().str.upper()
         
         
-        df_d_agrupado = df_deudas.groupby('nombre_apellido')['monto_deuda'].sum().reset_index()
+        df_d_agrupado = df_deudas.groupby('nombre')['monto_adeudado'].sum().reset_index()
 
         # 5. Cruce Final (Merge)
         # Unimos la tabla de socios con la tabla de deudas consolidada
-        df_final = pd.merge(df_socios, df_d_agrupado, on='nombre_apellido', how='left')
+        df_final = pd.merge(df_socios, df_d_agrupado, on='nombre', how='left')
         
     
     else:
         df_final = df_socios
-        df_final['monto_deuda'] = None
+        df_final['monto_adeudado'] = None
 
     # Si un socio no tiene deuda, ponemos 0 en lugar de vacío (NaN)
-    df_final['monto_deuda'] = df_final['monto_deuda'].fillna(0)
+    df_final['monto_adeudado'] = df_final['monto_adeudado'].fillna(0)
 
     # Retornamos solo las columnas que necesita nuestra base de datos
-    return df_final[['cuit', 'nombre_apellido', 'telefono', 'monto_deuda']]
+    return df_final[['cuit', 'nombre', 'telefono', 'monto_adeudado']]
